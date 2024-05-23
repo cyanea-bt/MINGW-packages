70ac34835b44d95cffd6e65ed3396114e6b66746
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2024-05-23 23:02:07 +0200

    update export script

--- export.sh
+++ export.sh
@@ -1,20 +1,47 @@
 #!/usr/bin/env bash
 
 #
-# create a backup of all installed packages
+# create a backup of all installed and compiled packages
 #
 
-# clean-up
-if [ -d "./database" ]; then
-  rm -rf ./database
+# setup for archive filenames
+if [[ $# -eq 0 ]] ; then
+  DATE=$(date "+%Y-%m-%d-%H.%M.%S")
+else
+  DATE=$(date "+%Y-%m-%d")
 fi
 
+# clean-up before
+if [[ -d "./database" ]]; then
+  rm -rf ./database
+fi
+mkdir ./database
+
 # list all native/database pkgs and download them from msys database
-PKG_LIST=$(pacman -Qqn | xargs echo)
-mkdir -p ./database
-pacman -Sw --cachedir ./database/ ${PKG_LIST}
+# make sure to run pacman in msys env
+if [[ $MSYSTEM != "MSYS" ]]; then 
+  /usr/bin/env MSYSTEM=MSYS /usr/bin/bash -l <<- "END1"
+  PKG_LIST="$(pacman -Qqn | xargs echo)"
+  pacman -Sw --noconfirm --cachedir ./database/ ${PKG_LIST}
+END1
+else
+  PKG_LIST="$(pacman -Qqn | xargs echo)"
+  pacman -Sw --noconfirm --cachedir ./database/ ${PKG_LIST}
+fi
 
-DATE=$(date "+%Y-%m-%d-%H.%M.%S")
+# check pacman exit code
+retVal=${?}
+if [[ $retVal -ne 0 ]]; then
+  echo "pacman ERROR! Exit: ${retVal}"
+  exit ${retVal}
+else
+  echo "pacman SUCCESS!"
+fi
+
+# run 7z in mingw64 since msys version is slow
+/usr/bin/env MSYSTEM=MINGW64 /usr/bin/bash -l << END2
 7z a -t7z -y -mx=9 -mhe=on -mmt=on -ms=on -mtc=on -mtm=on -mta=on "/opt/packages_${DATE}.7z" "database" "mingw32" "mingw64"
+END2
 
+# clean-up after
 rm -rf ./database
